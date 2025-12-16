1503f8cf9a060f4cdd285bccfbeb4214e6445635
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-16 23:57:48 +0530

    Content.jsx

---
+++ Content.jsx
@@ -0,0 +1,9 @@
+function Content() {
+  return (
+    <div style={{ textAlign: "center", margin: "40px 0", fontSize: "20px" }}>
+      This is Main Content
+    </div>
+  );
+}
+
+export default Content;
