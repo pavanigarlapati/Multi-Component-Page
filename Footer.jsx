f2513e3471836d2f599885b3a0bb278e9c5baa59
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-16 23:58:16 +0530

    Footer.jsx

---
+++ Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+  return (
+    <div style={{ backgroundColor: "#f1f1f1", color: "#555", padding: "10px", fontSize: "14px" }}>
+      This is Footer
+    </div>
+  );
+}
+
+export default Footer;
