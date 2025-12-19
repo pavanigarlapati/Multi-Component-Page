5b237d6a59a17c5d3f0aea82f2a3e290ce1c0f69
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-19 23:51:20 +0530

    Q1(3)

---
+++ dec19StatusToggle.jsx
@@ -0,0 +1,20 @@
+import { useState } from "react";
+import ComponentA from "./ComponentA";
+import ComponentB from "./ComponentB";
+
+function StatusToggle() {
+  const [status, setStatus] = useState(false);
+
+  return (
+    <div>
+      <button onClick={() => setStatus(!status)}>
+        Toggle Status
+      </button>
+
+      {/* Conditional Rendering */}
+      {status ? <ComponentA /> : <ComponentB />}
+    </div>
+  );
+}
+
+export default StatusToggle;
