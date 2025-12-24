266a0605704e31ad6aada559afdaa12f1791039f
pavanigarlapati <pavanigarlapati05@gmail.com>
2025-12-24 23:51:26 +0530

    dec24_Q1_Todos.jsx

---
+++ dec24_Q1_Todos.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function Todos() {
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then((res) => res.json())
+      .then((data) => setTodos(data.slice(0, 10)));
+  }, []);
+
+  return (
+    <div>
+      <h2>Todos</h2>
+      <div className="todo-grid">
+        {todos.map((todo) => (
+          <div key={todo.id} className="todo-card">
+            <h4>{todo.title}</h4>
+            <p>
+              Status: {todo.completed ? "Completed ✅" : "Not Completed ❌"}
+            </p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Todos;
